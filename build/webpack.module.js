b7ae7f0cdbc78795a8c4853642a49e52a68be99c
“NotLaughtingMe <1601534652@qq.com>
2018-02-07 11:27:21 +0800

    配置css，scss

--- build/webpack.module.js
+++ build/webpack.module.js
@@ -23,6 +23,11 @@ const rules = [
     }
   },
   {
+    test: /\.(css|scss)$/,
+    use: ["style-loader", "css-loader","postcss-loader","sass-loader"],
+    include: ROOT_PATH
+  },
+  {
     test: /\.(js)$/,
     exclude: /node_modules/,
     use: {
